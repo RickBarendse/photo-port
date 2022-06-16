852bcfbb307096dfbd2354e9b16d6508edf690b8
Rick Barendse <dazedeyes@yahoo.com>
2022-06-16 12:48:14 -0700

    add form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
-
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-    }
-
-    console.log(formState);
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formState);
+        if (!errorMessage) {
+            setFormState({ [e.target.name]: e.target.value })
+            console.log(formState);
+        }
+    };
+
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid');
+            } else {
+                setErrorMessage('');
+                }
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+            }
+        };
+        console.log('errorMessage', errorMessage);
     }
+
 
     return (
         <section>
@@ -21,20 +42,25 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+                    <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>    
+                )}
                 </div>
                 <button type="submit">Submit</button>
             </form>
         </section>
     );
-}
-
+};
+    
 export default ContactForm;
